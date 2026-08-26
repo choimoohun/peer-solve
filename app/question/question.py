e98702977ffb5989fdbf3ee314f08b4da7a6f29a
choimoohun <271946661+choimoohun@users.noreply.github.com>
2026-08-26 16:00:40 +0900

    코드 상세 조회 작업중

--- app/question/question.py
+++ app/question/question.py
@@ -99,6 +99,34 @@ def get_questions():
     return jsonify({"result": "success", "questions": questions}), 200
 
 
+@question_bp.route("/<question_id>", methods=["GET"])
+def get_question(question_id):
+    user = current_user()
+    if not user:
+        return jsonify({"result": "failure", "message": "로그인이 필요합니다."}), 401
+
+    group = current_group()
+    if not group:
+        return jsonify({"result": "failure", "message": "선택된 그룹이 없습니다."}), 400
+
+    if user["_id"] not in group["members"]:
+        return (
+            jsonify({"result": "failure", "message": "해당 그룹의 멤버가 아닙니다."}),
+            403,
+        )
+
+    question = db.question.find_one({}, {"_id": ObjectId(question_id), "group_id": group["_id"]})
+
+    if not question:
+        return jsonify({"result": "failure", "message": "코드를 찾지 못했습니다."}), 404
+
+    question["_id"] = str(question["_id"])
+    question["owner"] = str(question["owner"])
+    question["group_id"] = str(question["group_id"])
+
+    return jsonify({"result": "success", "question": question})
+
+
 @question_bp.route("/<question_id>/edit", methods=["GET"])
 def edit_question(question_id):
     user = current_user()
