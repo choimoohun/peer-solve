a239a4fa3be24993ef0bad73fa49dabb7f554eb0
Junho Park <nari40@kyonggi.ac.kr>
2026-08-26 11:01:03 +0900

    group_edit 기틀

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import sys
 from app import api_bp
 
 app = Flask(__name__, static_folder='app/static', template_folder='app/templates')
-#시크릿키 설정
+# 시크릿키 설정
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 # 블루프린트 등록 관리
 app.register_blueprint(api_bp)
@@ -19,6 +19,7 @@ def render_index():
 def render_main():
     return render_template('main.html')
 
+# 로그인
 @app.route('/signup')
 def render_signup():
     return render_template('auth/signup.html')
@@ -26,6 +27,15 @@ def render_signup():
 @app.route('/login')
 def render_login():
     return render_template('auth/login.html')
+
+# 그룹
+@app.route('/groupedit')
+def render_group_edit():
+    return render_template('group/gorup_edit.html')
+
+@app.route('/groupcreate')
+def render_group_create():
+    return render_template('auth/group_create.html')
 
 if __name__ == '__main__':
     print(sys.executable)
